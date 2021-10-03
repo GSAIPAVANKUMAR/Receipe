fc986e859d41a13e0fd2e7e445653edd6a32aa4e
GSAIPAVANKUMAR <saipavankumargunda@gmail.com>
2021-10-03 16:46:35 +0530

    Data binding

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output,EventEmitter } from '@angular/core';
 
+import { Recipe } from '../../recipe.model';
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeItemComponent implements OnInit {
 
+  @Input() recipe : Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelected(){
+    this.recipeSelected.emit();
+    console.log("item clicked");
   }
 
 }
