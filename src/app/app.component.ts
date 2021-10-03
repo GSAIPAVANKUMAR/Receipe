c41b3c7a1720c4236dbf592263c2018ee74f0af8
GSAIPAVANKUMAR <saipavankumargunda@gmail.com>
2021-10-03 11:17:01 +0530

    Navigation to different tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedOption: string = 'recipe';
+  navigate(option: string){
+      this.loadedOption = option;
+  }
 }
