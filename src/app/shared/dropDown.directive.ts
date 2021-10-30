beea6e06d407bfe12f59b943ff600cd25962b2ae
GSAIPAVANKUMAR <saipavankumargunda@gmail.com>
2021-10-30 15:38:23 +0530

    bug in dropdown functionality is fixed

--- src/app/shared/dropDown.directive.ts
+++ src/app/shared/dropDown.directive.ts
@@ -13,7 +13,7 @@
 import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
  
 @Directive({
-  selector: '[appDropdown]'
+  selector: '[appDropDown]'
 })
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
